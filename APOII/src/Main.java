f4c12a783b98142c73b818f4badf77c066c38c76
Juan Camilo Corrales Osvath <1007489003@u.icesi.edu.co>
2023-08-11 10:56:16 -0500

    clase 11 agosto

---
+++ APOII/src/Main.java
@@ -0,0 +1,97 @@
+import java.util.Scanner;
+
+public class Main {
+
+    private UserController controller;
+    private Scanner sc;
+
+    public Main(){
+        controller = new UserController();
+        sc = new Scanner(System.in);
+        initUsers();
+    }
+
+    public static void main(String[] args) {
+        Main m = new Main();
+        // variable para leer la entrada
+        int option= 0;
+
+        //ciclo para ejecutar el menu mientras que el usuario no
+        // elija la opciOn para salir (0)
+        do {
+            option = m.showMenuAndGetOption();
+            m.answerOption(option);
+        }while (option !=0);
+    }
+
+    /**
+     * Metodo que se encarga de llamar a los mEtodos que resuelven cada uno de los
+     * requerimientos de la aplicaciOn
+     * @param userOption, int es el nUmero ingresado por el usuario (no ha sido validado)
+     */
+    public void answerOption(int userOption) {
+        switch (userOption) {
+            case 0:
+                System.out.println("cerrando la aplicaciOn, bye");
+                break;
+            case 1:
+                addUser();
+                break;
+            case 2:
+                searchUser();
+                break;
+            case 3:
+                deleteUser();
+                break;
+            case 4:
+                printUsers();
+                break;
+            case 5:
+
+        }
+    }
+    private void deleteUser() {
+
+    }
+
+    private void searchUser() {
+
+    }
+
+    private void addUser() {
+
+    }
+
+    private void initUsers(){
+        for (int i = 0; i< 10; i++){
+            controller.addLastUser(new User(i));
+        }
+    }
+
+    private void printUsers(){
+        System.out.println(controller.print());
+    }
+
+    /**
+     * Metodo que muestra el menu de la aplicaciOn, a este metod hace falta implementar la validaciOn
+     * del valor ingresado por el usuario
+     * @return input, int es la opciOn elegida por el usuario
+     */
+    public int showMenuAndGetOption() {
+        int input;
+        System.out.println("\n\nMenu de la aplicación, digite una opciOn\n"+
+                "(1) agregar User\n" +
+                "(2) buscar User\n" +
+                "(3) eliminar User\n"+
+                "(4) opciOn 4\n"+
+                "(5) opciOn 5\n"+
+                "(0) Para salir"
+
+        );
+        input = sc.nextInt();
+        sc.nextLine();
+        return input;
+    }
+
+
+}
---
+++ APOII/src/UserController.java
@@ -0,0 +1,118 @@
+public class UserController {
+    private User head;
+
+    private User tail;
+
+    public UserController(){}
+
+    public User getHead() {
+        return head;
+    }
+
+    public User getTail() {
+        return tail;
+    }
+
+    public void addFirstUser(User user){
+        // when list is empty
+        if(this.head == null){
+            this.head = user;
+            this.tail = user;
+        }// already has users. add to the beginning.
+        else{
+            user.setNext(this.head);
+            this.head = user;
+        }
+    }
+
+    public void addLastUser(User user){
+
+        if(this.head == null){
+            this.head = user;
+            this.tail = user;
+        }else {
+            this.tail.setNext(user);
+            this.tail = user;
+        }
+    }
+    public String print(){
+        return print(this.head);
+    }
+    private String print(User current){
+        String str = "";
+        if(this.head == null){
+            str = "list is empty";
+        } else if (current.getNext() == null) {
+            str += " " + current.getId();
+        }else {
+           str += current.getId() + " " + print(current.getNext());
+        }
+        return str;
+    }
+
+    // sobreescritura de metodos : metodo de activacion
+    public String delete(int goal){
+        return delete(goal, this.head, null);
+    }
+
+    // metodo recursivo
+    private String delete(int goal, User current, User previous){
+        String str = "";
+
+        // cas base: la lista esta vacia
+        if(head == null){
+            str = "list is empty";
+
+        } // el elemento no existe
+
+        else if (current == null) {
+            str = "this element does not exist";
+        } // estamos en el nodo a eliminar
+        else if ( current.getId() == goal ) {
+            // sub caso base (caso borde)+
+            if(current.getId() == this.head.getId()){
+                // actuializamos el estado de la lista
+                this.head = current.getNext();
+                // desconectamos el elemento
+                current.setNext(null);
+            } else if (current.getId() == tail.getId()) {
+                // sub caso base (caso borde)+
+                // actuializamos el estado de la lista
+                this.tail = previous;
+                // desconectamos el elemento
+                previous.setNext(null);
+            } else {
+                // caso de un nodo intermedio
+                previous.setNext(current.getNext());
+                current.setNext(null);
+            }
+        } // caso recursivo
+        else {
+            delete(goal, current.getNext(), current);
+        }
+        return str;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
