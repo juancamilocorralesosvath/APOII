977c9727236c8cd35aaf72cc1118233b0827f7d8
Juan Camilo Corrales Osvath <1007489003@u.icesi.edu.co>
2023-08-30 08:35:26 -0500

    add delete method for the BST, modified the getmin method to return a node object.

--- APOII/src/model/BST.java
+++ APOII/src/model/BST.java
@@ -59,15 +59,121 @@ public class BST {
         }
     }
 
-    public String getMin(){
+    public TreeNode getMin(){
         return getMin(this.root);
     }
 
-    private String getMin(TreeNode current){
+    private TreeNode getMin(TreeNode current){
         if(current.getLeft() == null){
-            return current.getKey();
+            return current;
         }else {
             return getMin(current.getLeft());
         }
     }
+
+    /*
+    * vamos a hacer el metodo de eliminar:
+    *
+    * vamos a suponer que tenemos la referencia hacia el nodo padre,
+    * aun no sabemos como vamos a hacer para tenerlo.
+    * estoy en un nodo hoja
+    * if(current.left == null && current.right == null){
+    *   if(current == root){
+    *   root = null;
+    * }else if(parent.left == current){
+    *   parent.left = null;
+    * }else if(parent.right == current){
+    *   parent.right = null;
+    * }
+    *
+    *
+    * }
+    * */
+
+    public void delete(String goal){
+        delete(null, this.root, goal);
+    }
+    private void delete(TreeNode parent,TreeNode current, String goal){
+
+        if(current == null){
+            // el nodo no esta en el arbol
+
+        }
+
+        // cuando encontramos el nodo
+        if(goal.equals(current.getKey())){
+            // el nodo es hoja
+            if(current.getLeft() == null && current.getRight() == null){
+                // si es la raiz
+                if(current == this.root){
+                    this.root = null;
+                }// si es hijo izquierdo
+                else if(parent.getLeft() == current){
+                    parent.setLeft(null);
+                }// si es hijo derecho
+                else if(parent.getRight() == current){
+                    parent.setRight(null);
+                }
+
+
+            }// el nodo no es hoja, pero tiene unicamente un hijo izquierdo
+            else if(current.getLeft() != null && current.getRight() == null){
+                if(parent.getLeft() == current){
+                    parent.setLeft(current.getLeft());
+                } else if (parent.getRight() == current) {
+                    parent.setRight(current.getLeft());
+                }
+            }// el nodo tiene unicamente un hijo derecho
+            else if (current.getRight() != null && current.getLeft() == null) {
+                if(parent.getRight() == current){
+                    parent.setRight(current.getRight());
+                }else if(parent.getLeft() == current){
+                    parent.setLeft(current.getRight());
+                }
+            } // sucesor y predecesor: techo y piso
+            // tiene ambos hijos
+            else if (current.getLeft() != null && current.getRight() != null) {
+                // vamos a obtener el sucesor-> el menor de los mayores:
+                TreeNode successor = getMin(current.getRight());
+
+                // vamos a reemplazar los atributos en lugar de las conexiones, puesto que es mas sencillo:
+                current.setKey(successor.getKey());
+
+                // vamos a eliminar el successor, de esa manera, actualizamos las conexiones de manera implicita:
+                delete(current, current.getRight(), successor.getKey());
+            }
+        }// cuando aun no lo he econtrado y el valor es menor a mi actual (el nodo que busco debe estar hacia la izquierda)
+        else if(goal.compareTo(current.getKey()) < 0){
+            delete(current, current.getLeft(), goal);
+        } else if (goal.compareTo(current.getKey()) > 0) {
+            delete(current, current.getRight(), goal);
+        }
+
+    }
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
